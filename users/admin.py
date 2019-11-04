170a7b4e14a9d0769bc375c5e2349576b2e4a25e
EungangJi <eungangji@EungangJiui-MacBookPro.local>
2019-11-05 01:02:50 +0900

    can't understand how admin, model and app work together

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)  # decorator
+class CustomUserAdmin(admin.ModelAdmin):
+
+    """ Custom User Admin """
+
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = ("superhost", "language", "currency")
+
