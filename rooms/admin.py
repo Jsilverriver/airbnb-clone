c56cd492ce17bfec80eb210504058667560c60a9
EungangJi <eungangji@EungangJiui-MacBookPro.local>
2019-11-08 00:27:25 +0900

    still rooms app

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.RoomType)
+class itemAdmin(admin.ModelAdmin):
+
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
